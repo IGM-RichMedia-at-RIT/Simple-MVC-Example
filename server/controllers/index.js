48502dc16340f72ada5f55e77fa3d32a399c894b
Austin Willoughby <arwigm@rit.edu>
2019-02-25 10:40:58 -0500

    Revert "updated for assignment"
    This reverts commit 756fe59b252dc6f668368b5f2d8dd0a1f5f08651.

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,53 +1,28 @@
 // pull in our models. This will automatically load the index.js from that folder
 const models = require('../models');
 
-const Cat = models.Cat.CatModel;
-
 // default fake data so that we have something to work with until we make a real Cat
 const defaultData = {
   name: 'unknown',
   bedsOwned: 0,
 };
 
-// object for us to keep track of the last Cat we made and dynamically update it sometimes
-let lastAdded = new Cat(defaultData);
+let lastAdded;
 
 const hostIndex = (req, res) => {
-  res.render('index', {
-    currentName: lastAdded.name,
-    title: 'Home',
-    pageName: 'Home Page',
-  });
+
 };
 
 const readAllCats = (req, res, callback) => {
-  Cat.find(callback);
+
 };
 
 const readCat = (req, res) => {
-  const name1 = req.query.name;
-
-  const callback = (err, doc) => {
-    if (err) {
-      return res.json({ err });
-    }
-
-    return res.json(doc);
-  };
-
-  Cat.findByName(name1, callback);
+ 
 };
 
 const hostPage1 = (req, res) => {
-  const callback = (err, docs) => {
-    if (err) {
-      return res.json({ err }); // if error, return it
-    }
 
-    return res.render('page1', { cats: docs });
-  };
-
-  readAllCats(req, res, callback);
 };
 
 const hostPage2 = (req, res) => {
@@ -59,63 +34,24 @@ const hostPage3 = (req, res) => {
 };
 
 const getName = (req, res) => {
-  res.json({ name: lastAdded.name });
+
 };
 
 const setName = (req, res) => {
   if (!req.body.firstname || !req.body.lastname || !req.body.beds) {
     return res.status(400).json({ error: 'firstname,lastname and beds are all required' });
   }
-
-  const name = `${req.body.firstname} ${req.body.lastname}`;
-
-  // dummy JSON to insert into database
-  const catData = {
-    name,
-    bedsOwned: req.body.beds,
-  };
-
-  const newCat = new Cat(catData);
-
-  const savePromise = newCat.save();
-
-  savePromise.then(() => {
-    lastAdded = newCat;
-    res.json({ name: lastAdded.name, beds: lastAdded.bedsOwned });
-  });
-
-  savePromise.catch(err => res.json({ err }));
-
-  return res;
+  
 };
-
 
 const searchName = (req, res) => {
   if (!req.query.name) {
     return res.json({ error: 'Name is required to perform a search' });
   }
-
-  return Cat.findByName(req.query.name, (err, doc) => {
-    if (err) {
-      return res.json({ err }); // if error, return it
-    }
-
-    if (!doc) {
-      return res.json({ error: 'No cats found' });
-    }
-
-    return res.json({ name: doc.name, beds: doc.bedsOwned });
-  });
 };
 
 const updateLast = (req, res) => {
-  lastAdded.bedsOwned++;
-
-  const savePromise = lastAdded.save();
-
-  savePromise.then(() => res.json({ name: lastAdded.name, beds: lastAdded.bedsOwned }));
-
-  savePromise.catch(err => res.json({ err }));
+	
 };
 
 const notFound = (req, res) => {
