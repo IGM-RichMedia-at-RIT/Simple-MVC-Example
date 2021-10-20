a57083f3ed900ad4275291d84409acf60a330ae4
Austin Willoughby <arwigm@rit.edu>
2021-10-20 10:35:23 -0400

    Removed legacy code from Mongoose 5

--- server/app.js
+++ server/app.js
@@ -14,13 +14,7 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/simpleMVCExample';
 
-const mongooseOptions = {
-  useNewUrlParser: true, 
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-}
-
-mongoose.connect(dbURL, mongooseOptions, (err) => {
+mongoose.connect(dbURL, (err) => {
   if (err) {
     console.log('Could not connect to database');
     throw err;
